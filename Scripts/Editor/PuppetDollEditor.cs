9dcca619c855920db18c16a1ce0182a0955c147c
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-07-21 12:54:20 +0100

    attempt 1

--- Scripts/Editor/PuppetDollEditor.cs
+++ Scripts/Editor/PuppetDollEditor.cs
@@ -11,21 +11,35 @@ namespace freakycheesy.PuppetDoll.Editor
     public class PuppetDollEditor : UnityEditor.Editor
     {
         bool clearBones;
+        PuppetDoll doll;
         public override void OnInspectorGUI()
         {
-            PuppetDoll doll = (PuppetDoll)target;
+            doll = (PuppetDoll)target;
             base.OnInspectorGUI();
-            if (GUILayout.Button("Try Match Bones")) TryMatchBones(doll);
+            if (GUILayout.Button("Find Bones")) FindBones();
             clearBones = GUILayout.Toggle(clearBones, "Clear Bones");
             if (clearBones) if (GUILayout.Button("Clear Bones")) doll.bones.Clear();
         }
 
-        private void TryMatchBones(PuppetDoll doll)
+        private void FindBones()
         {
-            foreach (var bone in doll.bones)
+            var bones = new List<RagdollBone>();
+            var rigidbodies = doll.physicalHip.GetComponentsInChildren<Rigidbody>(true);
+            foreach (var rb in rigidbodies)
             {
-                bone.SetVirtualBone(GameObject.Find(bone.physicsBone.name).transform);
+                bones.Add(new(rb));
             }
+
+            var virtuals = doll.physicalHip.GetComponentsInChildren<Transform>(true);
+            foreach (var virtua in virtuals)
+            {
+                for (int i = 0; i < bones.Count; i++)
+                {
+                    if (bones[i].physicsBone.name == virtua.name) bones[i] = new(bones[i].physicsBone, virtua);
+                }
+            }
+
+            doll.bones.AddRange(bones);
         }
     }
 }
--- Scripts/Runtime/PuppetDoll.cs
+++ Scripts/Runtime/PuppetDoll.cs
@@ -6,22 +6,74 @@ namespace freakycheesy.PuppetDoll {
     [AddComponentMenu("PuppetDoll/Puppet Doll")]
     public class PuppetDoll : MonoBehaviour {
         public List<RagdollBone> bones;
-
-        public float inertiaTensorResitance = 0.2f;
-
-        public float proportionalGain = 1f;
-        public float derivativeGain = 0.1f;
-        public float maxStrength = 100f;
+        public Rigidbody physicalHip;
+        public Transform virtualHip;
+        public PIDSettings pidSettings = new(0.2f);
+        
         
         // Following sparksmints ragdoll doc
-        private void FixedUpdate() {
-            foreach (RagdollBone bone in bones) {
-                Torque(bone);
+        private void FixedUpdate()
+        {
+            foreach (RagdollBone bone in bones)
+            {
+                bone.Torque(pidSettings);
             }
         }
+        public static Quaternion GetRotationDelta(RagdollBone bone) {
+            // Gets the difference between the physics bone rot and the virtual bone rot.
+            // This is the Quaternion of getting a linear delta.
+            return bone.virtualBone.rotation *
+              Quaternion.Inverse(bone.physicsBone.rotation);
+        }
+    }
 
-        private void Torque(RagdollBone bone) {
-            Quaternion deltaRotation = GetRotationDelta(bone);
+    [Serializable]
+    public struct PIDSettings
+    {
+        public float inertiaTensorResitance;
+
+        public float proportionalGain;
+        public float derivativeGain;
+        public float maxStrength;
+
+        public PIDSettings(float inertiaTensorResitance, float proportionalGain, float derivativeGain, float maxStrength)
+        {
+            this.inertiaTensorResitance = inertiaTensorResitance;
+            this.proportionalGain = proportionalGain;
+            this.derivativeGain = derivativeGain;
+            this.maxStrength = maxStrength;
+        }
+        public PIDSettings(float inertiaTensorResitance = 0.2f)
+        {
+            this.inertiaTensorResitance = inertiaTensorResitance;
+            this.proportionalGain = 1f;
+            this.derivativeGain = 0.1f;
+            this.maxStrength = 100f;
+        }
+    }
+
+    [Serializable]
+    public struct RagdollBone
+    {
+        public Rigidbody physicsBone;
+        public Transform virtualBone;
+        public RagdollBone(Rigidbody physicsBone = null, Transform virtualBone = null)
+        {
+            this.physicsBone = physicsBone;
+            this.virtualBone = virtualBone;
+        }
+        public RagdollBone(Transform virtualBone = null, Rigidbody physicsBone = null)
+        {
+            this.virtualBone = virtualBone;
+            this.physicsBone = physicsBone;
+        }
+
+        public void SetPhysicsBone(Rigidbody newBone) => physicsBone = newBone;
+        public void SetVirtualBone(Transform newBone) => virtualBone = newBone;
+
+        public void Torque(PIDSettings pid)
+        {
+            Quaternion deltaRotation = PuppetDoll.GetRotationDelta(this);
 
             // Basically Absolute Value equivalent for the Quaternion.
             if (deltaRotation.w < 0)
@@ -35,28 +87,12 @@ namespace freakycheesy.PuppetDoll {
 
             Vector3 angularVelocityError = angle / Time.fixedDeltaTime * axis;
 
-            Vector3 torque = proportionalGain * angularVelocityError - derivativeGain *
-              bone.physicsBone.angularVelocity;
+            Vector3 torque = pid.proportionalGain * angularVelocityError - pid.derivativeGain *
+              physicsBone.angularVelocity;
 
-            torque = Vector3.ClampMagnitude(torque, maxStrength);
+            torque = Vector3.ClampMagnitude(torque, pid.maxStrength);
 
-            bone.physicsBone.AddTorque(torque, ForceMode.VelocityChange);
+            physicsBone.AddTorque(torque, ForceMode.VelocityChange);
         }
-
-        private static Quaternion GetRotationDelta(RagdollBone bone) {
-            // Gets the difference between the physics bone rot and the virtual bone rot.
-            // This is the Quaternion of getting a linear delta.
-            return bone.virtualBone.rotation *
-              Quaternion.Inverse(bone.physicsBone.rotation);
-        }
-    }
-
-    [Serializable]
-    public struct RagdollBone {
-        public Rigidbody physicsBone;
-        public Transform virtualBone;
-
-        public void SetPhysicsBone(Rigidbody newBone) => physicsBone = newBone;
-        public void SetVirtualBone(Transform newBone) => virtualBone = newBone;
     }
 }
