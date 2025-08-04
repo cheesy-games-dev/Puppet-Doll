fbbbc3967c3bfdf3bfd35e9abb316c71d1da5780
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-08-04 14:08:45 +0100

    Update PuppetDoll.cs

--- Scripts/Runtime/PuppetDoll.cs
+++ Scripts/Runtime/PuppetDoll.cs
@@ -2,11 +2,9 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace freakycheesy.PuppetDoll
-{
+namespace freakycheesy.PuppetDoll {
     [AddComponentMenu("PuppetDoll/Puppet Doll")]
-    public class PuppetDoll : MonoBehaviour
-    {
+    public class PuppetDoll : MonoBehaviour {
         public List<RagdollBone> bones;
         public Rigidbody physicalHip;
         public Transform virtualHip;
@@ -17,17 +15,16 @@ namespace freakycheesy.PuppetDoll
 
 
         // Following sparksmints ragdoll doc
-        private void FixedUpdate()
-        {
-            if (virtuaHipFollowPhysicalHip) virtualHip.position = physicalHip.position;
-            if (virtuaHipRotatePhysicalHip) virtualHip.rotation = physicalHip.rotation;
-            foreach (RagdollBone bone in bones)
-            {
+        private void FixedUpdate() {
+            if (virtuaHipFollowPhysicalHip)
+                virtualHip.position = physicalHip.position;
+            if (virtuaHipRotatePhysicalHip)
+                virtualHip.rotation = physicalHip.rotation;
+            foreach (RagdollBone bone in bones) {
                 Torque(bone);
             }
         }
-        public void Torque(RagdollBone bone)
-        {
+        public void Torque(RagdollBone bone) {
             bone.physicsBone.inertiaTensor = Vector3.one * pidSettings.inertiaTensorResitance;
             Quaternion deltaRotation = GetRotationDelta(bone);
 
@@ -48,10 +45,9 @@ namespace freakycheesy.PuppetDoll
 
             torque = Vector3.ClampMagnitude(torque, pidSettings.maxStrength);
 
-            bone.physicsBone.AddTorque(torque, ForceMode.Force);
+            bone.physicsBone.AddTorque(torque, forceMode);
         }
-        public static Quaternion GetRotationDelta(RagdollBone bone)
-        {
+        public static Quaternion GetRotationDelta(RagdollBone bone) {
             // Gets the difference between the physics bone rot and the virtual bone rot.
             // This is the Quaternion of getting a linear delta.
             return bone.virtualBone.rotation *
@@ -60,23 +56,20 @@ namespace freakycheesy.PuppetDoll
     }
 
     [Serializable]
-    public struct PIDSettings
-    {
+    public struct PIDSettings {
         public float inertiaTensorResitance;
 
         public float proportionalGain;
         public float derivativeGain;
         public float maxStrength;
 
-        public PIDSettings(float inertiaTensorResitance, float proportionalGain, float derivativeGain, float maxStrength)
-        {
+        public PIDSettings(float inertiaTensorResitance, float proportionalGain, float derivativeGain, float maxStrength) {
             this.inertiaTensorResitance = inertiaTensorResitance;
             this.proportionalGain = proportionalGain;
             this.derivativeGain = derivativeGain;
             this.maxStrength = maxStrength;
         }
-        public PIDSettings(float inertiaTensorResitance = 0.2f)
-        {
+        public PIDSettings(float inertiaTensorResitance = 0.2f) {
             this.inertiaTensorResitance = inertiaTensorResitance;
             this.proportionalGain = 1f;
             this.derivativeGain = 0.1f;
@@ -85,17 +78,14 @@ namespace freakycheesy.PuppetDoll
     }
 
     [Serializable]
-    public struct RagdollBone
-    {
+    public struct RagdollBone {
         public Rigidbody physicsBone;
         public Transform virtualBone;
-        public RagdollBone(Rigidbody physicsBone = null, Transform virtualBone = null)
-        {
+        public RagdollBone(Rigidbody physicsBone = null, Transform virtualBone = null) {
             this.physicsBone = physicsBone;
             this.virtualBone = virtualBone;
         }
-        public RagdollBone(Transform virtualBone = null, Rigidbody physicsBone = null)
-        {
+        public RagdollBone(Transform virtualBone = null, Rigidbody physicsBone = null) {
             this.virtualBone = virtualBone;
             this.physicsBone = physicsBone;
         }
